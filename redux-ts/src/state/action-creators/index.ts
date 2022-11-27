308060bc88ab3b9928a7d08622615d738162a53b
Tabby Toney Douglass <tabbytoney@gmail.com>
2022-11-27 14:37:28 -0600

    Creating action creator

---
+++ redux-ts/src/state/action-creators/index.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+// using thunk to create an asynchronous action creator
+// dispatch = how we manually dispatch actions directly into the redux store and get it processed by a reducer
+export const searchRepositories = (term: string) => {
+  return async (dispatch: any) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES,
+    });
+
+    try {
+      const { data } = await axios.get(
+        'https://registry.npmjs.org/-/v1/search',
+        {
+          // params is what's going to add our query term to the above url
+          params: {
+            text: term,
+          },
+        }
+      );
+      // objects comes from the api, it's what contains all the data
+      // package.name are inside the objects objects, that's the info we want to display
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        dispatch({
+          type: ActionType.SEARCH_REPOSITORIES_ERROR,
+          payload: err.message,
+        });
+      }
+    }
+  };
+};
