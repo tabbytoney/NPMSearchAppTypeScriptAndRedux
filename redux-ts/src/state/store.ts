13a06f8354dfb7fd09d1ab17c84bfe2cea770bd1
Tabby Douglass <tabbytoney@gmail.com>
2022-11-29 11:02:33 -0600

    set up store

--- redux-ts/src/state/store.ts
+++ redux-ts/src/state/store.ts
@@ -2,5 +2,5 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-// {} is the initial state objext
-export const store = createStore(reducers, {});
+// {} is the initial state object
+export const store = createStore(reducers, {}, applyMiddleware(thunk));
